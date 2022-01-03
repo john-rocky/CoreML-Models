543d42afcf5ae1a3ba9583a89cd7ecd1fb0d9c02
MLBoy <rockyshikoku@gmail.com>
2022-01-04 02:33:56 +0900

    Add files via upload

---
+++ CoreML-Models/CoreML-Models/CoreML_ModelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreML_ModelsApp.swift
+//  CoreML-Models
+//
+//  Created by 間嶋大輔 on 2022/01/04.
+//
+
+import SwiftUI
+
+@main
+struct CoreML_ModelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoreML-Models/CoreML-ModelsUITests/CoreML_ModelsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CoreML_ModelsUITestsLaunchTests.swift
+//  CoreML-ModelsUITests
+//
+//  Created by 間嶋大輔 on 2022/01/04.
+//
+
+import XCTest
+
+class CoreML_ModelsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
